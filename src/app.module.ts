0dc282cc23ab1afd9eca3d2b58c45ca71c2a53db
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-17 22:32:57 -0500

    connect nest with MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
