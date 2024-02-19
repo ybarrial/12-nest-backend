a9d1139a4e760da01db8dd37fa88a4689d1ad882
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-18 19:03:28 -0500

    add environments & @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot( process.env.MONGO_URI ),
     AuthModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+  constructor() {
+  }
+
+}
