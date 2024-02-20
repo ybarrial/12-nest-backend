aa557f1100f40ea4daced633cc9559e97bdee285
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-19 20:06:42 -0500

    save a register in Data Base

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    console.log( createUserDto )
-    return 'This action adds a new auth';
+
+  constructor(
+
+    // CON ESTE MODELO YO YA PUEDO HACER TODAS LAS INTERACCIONES A LA BASE DE DATOS RELACIONADA O
+    // -TODO LO QUE TENGA DEFINIDO EN ESE SCHEMA.
+    @InjectModel( User.name )
+    private userModel: Model<User>
+    // END
+
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    // console.log( createUserDto )
+    try {
+      const newUser = new this.userModel( createUserDto );
+      // 1- Encriptar la contrasena
+      // 2- Guardar el usuario
+      // 3- Generar el JWT
+
+      return await newUser.save();
+    } catch (error: any) {
+      console.log(error.code)
+      if ( error.code  === 11000 ) {
+        throw new BadRequestException( `${ createUserDto.email } already exists!` );
+      }
+      throw new InternalServerErrorException('Something terrible happen!!!');
+    }
+
   }
 
   findAll() {
