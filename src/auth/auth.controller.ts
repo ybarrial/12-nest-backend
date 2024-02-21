c5e753fbba3e727dca63f48dd3000cb3d71f2c05
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-21 11:39:15 -0500

    add method register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
-import { LoginDto } from './dto/login.dto';
+
+import { CreateUserDto, LoginDto, UpdateAuthDto, RegisterUserDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
@@ -16,6 +15,11 @@ export class AuthController {
   @Post('/login')
   login( @Body() loginDto: LoginDto ) {
     return this.authService.login(loginDto);
+  }
+
+  @Post('/register')
+  register( @Body() registerDto: RegisterUserDto ) {
+    return this.authService.register(registerDto);
   }
 
   @Get()
