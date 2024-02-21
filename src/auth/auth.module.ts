db0d4500cd9c02daa695a0e5ded5639ef1b6933e
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-21 09:57:20 -0500

    add jwt library & config of  jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,14 +4,24 @@ import { AuthController } from './auth.controller';
 import { Mongoose } from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
+    ConfigModule.forRoot(),
+
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
-    ])
+    ]),
+
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SEED,
+      signOptions: { expiresIn: '6H' },
+    }),
   ]
 })
 export class AuthModule {}
