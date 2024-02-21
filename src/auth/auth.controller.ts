932ff0b3d300cbd653c8a56fa591d07fb7b7cd4f
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-21 14:12:44 -0500

    user auth guard in findAll() in controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 import { CreateUserDto, LoginDto, UpdateAuthDto, RegisterUserDto } from './dto';
+import { AuthGuard } from './guards/auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -22,6 +23,7 @@ export class AuthController {
     return this.authService.register(registerDto);
   }
 
+  @UseGuards( AuthGuard )
   @Get()
   findAll() {
     return this.authService.findAll();
