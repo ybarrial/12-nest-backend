0fdf693173183cf87de15f2e35c7e4f0043055c3
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-29 20:36:07 -0500

    Port log added

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,9 @@ async function bootstrap() {
     }) 
   );
 
-  await app.listen( process.env.PORT ?? 3000);
+  const PORT = process.env.PORT ?? 3000;
+  console.log(` App corriendo en puerto: ${PORT}`)
+
+  await app.listen(PORT);
 }
 bootstrap();
